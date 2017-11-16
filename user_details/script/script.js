b19397ade0010d55f633acb0f227a00af9fe9f6f
damodarnaik65 <damodarnaik65@gmail.com>
2017-11-16 17:39:25 +0530

    fetched json data successfully

--- user_details/script/script.js
+++ user_details/script/script.js
@@ -2,10 +2,10 @@
 $(document).ready(function () {
     $("#get").click(function () {
         console.log("hi");
-        var resultElement=$('#pan2');
-        resultElement.html(' ');
-
-            var requestData=$('#btn1').val();
+        var results=$('#result');
+        var images=$('#img');
+        results.html(' ');
+        var requestData=$('#btn1').val();
         $.ajax({
             url: 'https://www.apimint.com:443/mock/ECATERING/v1/users/',
             headers: {
@@ -16,12 +16,46 @@ $(document).ready(function () {
             },
             method: 'GET',
             dataType: 'json',
-            //data: { q:data },
             success: function(data){
-                document.getElementById("pan2").innerHTML=+data;
+                
 
-              console.log('succes: '+data);
-              
+              console.log(data.user_details[0]);
+              console.log(data.user_details[1]);
+              results.html('Email : '+data.user_details[0].email+ '<br>' + 'Age : '+data.user_details[0].age + '<br>' + 'ID : '+data.user_details[0].email+ '<br>'+ 'Name : '+data.user_details[0].name);
+              images.html('<li><img src='+data.user_details[0].image+'></li>')
+            }
+          });
+   
+        
+    });
+});
+
+
+$(document).ready(function () {
+    $("#get1").click(function () {
+        console.log("hi");
+        var results=$('#result');
+        var images=$('#img');
+        results.html(' ');
+        var requestData=$('#btn1').val();
+        $.ajax({
+            url: 'https://www.apimint.com:443/mock/ECATERING/v1/users/',
+            headers: {
+                //'Authorization':'Basic xxxxxxxxxxxxx',
+                //'X_CSRF_TOKEN':'xxxxxxxxxxxxxxxxxxxx',
+                'x-mock-access': '20842df23128ea0b4ff651ff4572c0',
+                'Content-Type':'application/json'
+            },
+            method: 'GET',
+            dataType: 'json',
+            success: function(data){
+                
+
+              console.log(data.user_details[0]);
+              console.log(data.user_details[1]);
+              results.html('Email : '+data.user_details[1].email+ '<br>' + 'Age : '+data.user_details[1].age + '<br>' + 'ID : '+data.user_details[1].email+ '<br>'+ 'Name : '+data.user_details[1].name);
+              images.html('<li><img src='+data.user_details[1].image+'></li>')
+               
             }
           });
    
